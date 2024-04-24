0fbf101ecad383aaabf0d28bf4837a82ea43b0f2
Aula Faridah <aulafaridah@gmail.com>
2024-04-24 10:56:20 +0700

    ViewModel

--- PlacesAI/Model/Constant.swift
+++ PlacesAI/Model/Constant.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Content {
+struct Constant {
     static let apiKey = "AIzaSyBlDpxgjOuyDjkWHnVHrP0XBoxt0uPT2vM"
     static let prompt = """
     Give me inspiration for activities that can be done during the day in the city of Malang. Provide the response as an array of JSON as 
---
+++ PlacesAI/ViewModel/PlaceVM.swift
@@ -0,0 +1,36 @@
+//
+//  PlaceVM.swift
+//  PlacesAI
+//
+//  Created by MacBook Pro on 24/04/24.
+//
+
+import Foundation
+import GoogleGenerativeAI
+
+class PlaceVM: ObservableObject {
+    private var geminiModel: GenerativeModel
+    @Published var place: [Place] = []
+    
+    init() {
+        self.geminiModel = GenerativeModel(name: "gemini-pro", apiKey: Constant.apiKey)
+    }
+    
+    func getPlasces() async {
+        let prompt = Constant.prompt
+        
+        do {
+            let response = try await geminiModel.generateContent(prompt)
+            guard let text = response.text, let data = text.data(using: .utf8) else {
+                print("🤪 Unable to extract text or convert to data")
+                return
+            }
+            
+            place = try JSONDecoder().decode([Place].self, from: data) // Process of decoding
+            
+        } catch {
+            print("⚠️ ERROR fetching places: \(error.localizedDescription)")
+        }
+    }
+    
+}
